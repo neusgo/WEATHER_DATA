f254611b739a017c2d9cd60d91339f39b5890bc5
neusgo <neusmarianelgo@gmail.com>
2023-11-14 10:58:08 -0500

    Change in port,modification in post an get un app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
-@app.route('/weatherapp', methods = ("POST", "GET"))
+@app.route("/weatherapp", methods = ["POST", "GET"])
 def get_weatherdata():
     url= 'http://api.openweathermap.org/data/2.5/weather'
 
-    param = { 'q' : request.form.get("city"),
-         'units' : request.form.get("units"),
-         'appid' : request.form.get("appid")}
+    params = { 'q' : request.form.get("city"),
+         'appid' : request.form.get("appid"),
+         'units' : request.form.get("units")}
 
-    response = requests.get(url, params =param)
+    response = requests.get(url, params =params)
     data = response.json()
     city = data['name']
-    return f"data: {data}"
+    return f"data: {data}, city:{city}"
 
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5004)
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 5023)
